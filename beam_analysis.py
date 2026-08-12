93d0348e1b3d8a3c50143eb037e3fef38bd144ab
szaian <122959313+szaian@users.noreply.github.com>
2026-08-12 12:57:15 +0200

    Plotting raw data

--- beam_analysis.py
+++ beam_analysis.py
@@ -3,3 +3,22 @@ from astropy.io import fits
 filename = '20260804-113216_TPI-CSCAN_LON-SUN_01#_01#.fits'
 hdul = fits.open(filename)
 hdul.info()
+tab = hdul[1]
+print(tab.columns)
+print(tab.data['LEFT_POL'])
+print(tab.data['Az_Offset'])
+print(tab.data['El_Offset'])
+
+import matplotlib.pyplot as plt
+
+az_offset = tab.data['Az_Offset']
+left_pol = tab.data['LEFT_POL']
+
+plt.plot(az_offset, left_pol, 'o-')
+
+plt.xlabel('Azimuth Offset (degrees)')
+plt.ylabel('Total Power (counts)')
+plt.title('Sun Cross-Scan: Azimuth')
+
+plt.grid()
+plt.show()
